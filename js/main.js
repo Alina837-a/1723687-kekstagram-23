f9fbd74dfa8c641f10ec268f8167ebf147c91b4f
Alina-837-a <ALISHAKURITSYNA@yandex.ru>
2021-07-22 07:44:02 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-
-import {getPicturesContainer} from './mini-images.js';
-import {setUploadFormSubmit, onCloseFormPopup} from './form.js';
-import {removeFiltersHidden, onFiltersClick} from './filter.js';
-import {getData} from './api.js';
+import './util.js';
+import './data.js';
+import './draw-thumbnails.js';
 import './big-picture.js';
 import './form.js';
-
-getData((images) => {
-  getPicturesContainer(images);
-  removeFiltersHidden();
-  onFiltersClick(images);
-});
-setUploadFormSubmit(onCloseFormPopup);
